07172fc7f47b66bb2d46d4d7ab60db359c848b17
zoercai <zoercai@gmail.com>
2017-10-23 23:28:56 +1300

    use promise to update location - really slow

--- src/List.js
+++ src/List.js
@@ -7,17 +7,7 @@ export class List extends React.Component {
   @observable todos = [];
   previousId = 1;
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      current: "",
-      count: 0,
-      lat: undefined,
-      long: undefined
-    };
-
-    var positionOptions = {
+  positionOptions = {
       // enableHighAccuracy = should the device take extra time or power to return a really accurate result, or should it give you the quick (but less accurate) answer?
       enableHighAccuracy: false,
       // timeout = how long does the device have, in milliseconds to return a result?
@@ -26,73 +16,101 @@ export class List extends React.Component {
       maximumAge: 0
     };
 
-    this.onPositionSuccess = this.onPositionSuccess.bind(this);
-    this.onPositionError = this.onPositionError.bind(this);
+    constructor(props) {
+      super(props);
 
-    if (!navigator.geolocation || !navigator.geolocation.getCurrentPosition) {
-      throw new Error('The provided geolocation provider is invalid');
+      this.state = {
+        current: "",
+        count: 0,
+        lat: null,
+        long: null
+      };
+
+      this.getCurrentLocation = this.getCurrentLocation.bind(this);
+
+      if (!navigator.geolocation || !navigator.geolocation.getCurrentPosition) {
+        throw new Error('The provided geolocation provider is invalid');
+      }
+
+      this.getCurrentLocation()
+      .then(pos => {this.setState({ lat: pos.coords.latitude.toFixed(20), long: pos.coords.longitude.toFixed(20) })})
+      .catch(err => console.error("Oh no"));
+
+      // const cachedTodos = JSON.parse(localStorage.getItem('todos'));
+      // if (cachedTodos) {
+      //   this.todos = JSON.parse(localStorage.getItem('todos'));
+      // }
     }
-    navigator.geolocation.getCurrentPosition(this.onPositionSuccess, this.onPositionError, positionOptions);
 
-    const cachedTodos = JSON.parse(localStorage.getItem('todos'));
-    if (cachedTodos) {
-      this.todos = JSON.parse(localStorage.getItem('todos'));
+    getCurrentLocation() {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, this.positionOptions);
+        console.warn("something inside the promise");
+      })
     }
-  }
 
-  onPositionSuccess(pos) {
-    console.log(pos.coords.latitude, pos.coords.longitude);
-    this.setState({ lat: pos.coords.latitude.toFixed(20), long: pos.coords.longitude.toFixed(20)});
-  }
+    add(todoText) {
+      this.getCurrentLocation()
+      .then(pos => {
+        this.previousId++;
+        this.todos.push({id: this.previousId, text: todoText, lat: pos.coords.latitude.toFixed(20), long: pos.coords.longitude.toFixed(20)});
+        console.warn("got this lat: " + this.state.lat);
+        this.props.sendCount(this.getCount);
+        // localStorage.setItem('todos', JSON.stringify(this.todos));
+      })
+      .catch(err => console.warn("Could not get location."))
+    }
 
-  onPositionError(error) {
-    console.error("Oh no");
-  }
+    removeItem(id) {
+      this.todos = this.todos.filter(
+        function(item) {
+          return item.id !== id;
+        }.bind(this)
+        );
+      this.props.sendCount(this.getCount);
+      // localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
 
-  add(todoText, lat, long) {
-    // if (this.state.lat === undefined || this.state.long === undefined) {
-    //   console.log("dafjf");
-    //   return;
-    // }
-    this.previousId++;
-    this.todos.push({id: this.previousId, text: todoText, lat: this.state.lat, long: this.state.long});
-    console.log(this.state.lat);
-    this.props.sendCount(this.getCount);
-    localStorage.setItem('todos', JSON.stringify(this.todos));
-  }
+    @computed get getCount() {
+      return this.todos.length;
+    }
 
-  removeItem(id) {
-    this.todos = this.todos.filter(
-      function(item) {
-        return item.id !== id;
-      }.bind(this)
-    );
-    this.props.sendCount(this.getCount);
-    localStorage.setItem('todos', JSON.stringify(this.todos));
-  }
+    arePointsClose(lat1, lat2, lon1, lon2) {
+      function degrees_to_radians(degrees) {
+        var pi = Math.PI;
+        return degrees * (pi/180);
+      }
+      var accuracy = 10;
+      lat1 = degrees_to_radians(lat1);
+      lat2 = degrees_to_radians(lat2);
+      lon1 = degrees_to_radians(lon1);
+      lon2 = degrees_to_radians(lon2);
+      var distance = Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon1-lon2)) * 6371000;
+      console.warn("DISTANCE IS : " + distance);
+      return (distance < accuracy && distance > -accuracy);
+    }
 
-  @computed get getCount() {
-    return this.todos.length;
-  }
+    render() {
+      var items = this.todos.map(
+        function(item) {
+          console.warn("rendering" + this.state.lat);
+          console.warn(this.state.lat + " " + this.state.long);
+          console.warn(item.lat + " " + item.long);
+          if (this.arePointsClose(this.state.lat, item.lat, this.state.long, item.long)) {
+            return (
+              <div className="item ui checkbox" key={item.id}>
+              <input type="checkbox" name="example" onClick={() => this.removeItem(item.id)} />
+              <label>{item.text}: {item.lat}, {item.long}</label>
+              </div>
+              );
+          }
+        }.bind(this)
+        );
 
-  render() {
-    var items = this.todos.map(
-      function(item) {
-        if (item.lat == this.state.lat && item.long == this.state.long) {
-          return (
-            <div className="item ui checkbox" key={item.id}>
-                <input type="checkbox" name="example" onClick={() => this.removeItem(item.id)} />
-                <label>{item.text}: {item.lat}, {item.long}</label>
-            </div>
-            );
-        }
-      }.bind(this)
-    );
-
-    return (
-      <div className="ui huge celled list">
+      return (
+        <div className="ui huge celled list">
         {items}
-      </div>
-    );
-  }
-}
+        </div>
+        );
+      }
+    }
